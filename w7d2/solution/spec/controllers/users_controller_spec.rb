fa83aabdea55dbf30954ab391ef13459cfa98287
Alyx-Clark <alexclark.edu@gmail.com>
2022-01-19 19:44:56 -0500

    update

--- w7d2/solution/spec/controllers/users_controller_spec.rb
+++ w7d2/solution/spec/controllers/users_controller_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+
+  describe "GET #new" do
+    it "renders the new template" do
+      get :new, {}
+      
+    end
+  end
   describe "POST #create" do
     context "with invalid params" do
       it "validates the presence of the user's email and password" do
--- w7d2/solution/spec/models/user_spec.rb
+++ w7d2/solution/spec/models/user_spec.rb
@@ -6,7 +6,7 @@ describe User do
       email: "jonathan@fakesite.com",
       password: "good_password")
   end
-
+  
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:password_digest) }
   it { should validate_length_of(:password).is_at_least(6) }
